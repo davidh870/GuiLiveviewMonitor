5947cd5abe8b8c7a9a7eca8c979c7fb2da4a4cfc
davidh870 <davidh870@gmail.com>
2023-11-10 22:43:21 -0600

    update gui v2

--- arduinoReadData.py
+++ arduinoReadData.py
@@ -4,33 +4,13 @@ import PySimpleGUI as sg
 ports = serial.tools.list_ports.comports() # Reads ports in computer
 serialInst = serial.Serial() # Arduino
 
-portList = [] # List of ports
 
+# Iterate through the ports and append to string
+portListStrings = "List of Ports:\n"
 
-
-"""
-Setup serial connection with Arduino
-"""
-
-# Iterate through the ports and append to list
-print("List of Ports:")
 for port in ports:
-    portList.append(str(port)) 
-    print(str(port))
+    portListStrings += str(port) + '\n'
 
-# Ask for port selection
-portSelected = input("Select Port: COM")
-
-for x in range(0, len(portList)):
-        if portList[x].startswith("COM" + str(portSelected)):
-              portSelected = "COM" + str(portSelected) # Update to correct port
-              print("Port Selected: " + portList[x])
-
-
-# Setup up serial connection of arduino
-serialInst.baudrate = 9600
-serialInst.port = portSelected
-serialInst.open()
 
 
 
@@ -38,26 +18,57 @@ serialInst.open()
 Create GUI for live view data
 """
 # Create layout
+portListL = sg.Text(portListStrings)
+inputTitleL = sg.Text('Enter Com Port Number', key='-ECPN-')
+inputValueL = sg.Input('', enable_events=True, key ='-COMIN-')
+comOkButL = sg.Button('Ok', key='-OK-', bind_return_key=True)
+pvL = sg.Text("Photoresistor Value", key='-PV-')
+
 layout = [
-      [sg.Text("Photoresistor Value", key = 'pv')]
+    [portListL],
+    [inputTitleL],
+    [inputValueL],  
+    [comOkButL],
+    [pvL]
 ]
 
 # Create the window
 window = sg.Window("Live View Monitor", layout)
 
 
+portCreated = False # 
+
 while True:
-    # Read serial from arduino
-    if serialInst.in_waiting:
-        # Read any event (e.g. End program if user closes window)
-        event, values = window.read(timeout=1) 
+    # Setup up serial connection of arduino if haven't
+    if portCreated == False:
+        
+
+        # Ask for port selection
+        event, values = window.read(timeout=1)
+
         if event == sg.WIN_CLOSED:
             break
 
-        packet = serialInst.readline()
-        window['pv']("Photoresistor Value " + packet.decode('utf'))
-        print(packet.decode('utf').rstrip('\n'))
+        # Get user input of com port and setup connection if there is a value in input box
+        if event == '-OK-' and len(values['-COMIN-']):
+            serialInst.port = 'COM' + str(window['-COMIN-'].get())
+            serialInst.baudrate = 9600
+            serialInst.open()
+            portCreated = True
 
-window.close()
+    # Read serial once connection has been made
+    else:
+        # Read serial from arduino
+        if serialInst.in_waiting:
+            # Read any event (e.g. End program if user closes window)
+            event, values = window.read(timeout=1) # Add a time out of 1 millisecond to exit of window.read()
+            if event == sg.WIN_CLOSED:
+                break
 
+            # Retrieve serial packet from arduino and update gui
+            packet = serialInst.readline()
+            window['-PV-']("Photoresistor Value " + packet.decode('utf'))
+            print(packet.decode('utf').rstrip('\n')) # Print to terminal
+
+window.close() # Close window once user closes window and exit program
 
