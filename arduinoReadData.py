c9a991ab8683ebb44cc02baa429f7b4d04f1160a
davidh870 <davidh870@gmail.com>
2023-11-17 01:33:56 -0600

    updated gui with com port dropdown list

--- arduinoReadData.py
+++ arduinoReadData.py
@@ -5,56 +5,63 @@ ports = serial.tools.list_ports.comports() # Reads ports in computer
 serialInst = serial.Serial() # Arduino
 
 
+
+"""""""""""""""""""""""""""""""""""""""""
+Create COM PORT LIST AND STRINGS 
+"""""""""""""""""""""""""""""""""""""""""
 # Iterate through the ports and append to string
 portListStrings = "List of Ports:\n"
+portNumList = []
 
 for port in ports:
+    portNumList.append(str(port)[3])
     portListStrings += str(port) + '\n'
+""""""""""""""""""""""""""""""""""""""""""
 
 
-
-
-"""
+"""""""""""""""""""""""""""""""""""""""""
 Create GUI for live view data
-"""
+"""""""""""""""""""""""""""""""""""""""""
 # Create layout
-portListL = sg.Text(portListStrings)
-inputTitleL = sg.Text('Enter Com Port Number', key='-ECPN-')
-inputValueL = sg.Input('', enable_events=True, key ='-COMIN-')
-comOkButL = sg.Button('Ok', key='-OK-', bind_return_key=True)
-pvL = sg.Text("Photoresistor Value", key='-PV-')
-
 layout = [
-    [portListL],
-    [inputTitleL],
-    [inputValueL],  
-    [comOkButL],
-    [pvL]
+    [sg.Text(portListStrings, key='-PORTLIST-')],
+    [sg.Text('Enter Com Port Number', key='-ECPN-'), sg.Combo(portNumList, key='-COMLIST-')],  
+    [sg.Button('Ok', key='-OK-', bind_return_key=True)],
+    [sg.Text("Photoresistor Value", key='-PV-', visible=False)]
 ]
+
 
 # Create the window
 window = sg.Window("Live View Monitor", layout)
+""""""""""""""""""""""""""""""""""""""""""
 
 
+
+
+# Flag to check if connection with ardiuno has been made
 portCreated = False # 
 
 while True:
     # Setup up serial connection of arduino if haven't
     if portCreated == False:
-        
-
-        # Ask for port selection
         event, values = window.read(timeout=1)
 
         if event == sg.WIN_CLOSED:
             break
 
-        # Get user input of com port and setup connection if there is a value in input box
-        if event == '-OK-' and len(values['-COMIN-']):
-            serialInst.port = 'COM' + str(window['-COMIN-'].get())
+        # Get user input of com port and setup connection if there is a item selected in com port list
+        if event == '-OK-' and len(str(window['-COMLIST-'].get())):
+            serialInst.port = 'COM' + str(window['-COMLIST-'].get())
             serialInst.baudrate = 9600
             serialInst.open()
             portCreated = True
+
+            # Clear out gui to only display live data from arduino
+            window['-ECPN-'].update(visible=False)
+            window['-COMLIST-'].update(disabled=True, visible=False)
+            window['-OK-'].update(disabled=True, visible=False)
+            window['-PV-'].update(visible=True)
+
 
     # Read serial once connection has been made
     else:
