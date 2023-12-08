525b4cd784ff51cb752b24d8b2ce26bbbaa738af
davidh870 <davidh870@gmail.com>
2023-12-08 00:25:58 -0600

    Added record and stop for multiple data logs

--- arduinoReadData.py
+++ arduinoReadData.py
@@ -1,7 +1,8 @@
 import serial.tools.list_ports
 import PySimpleGUI as sg
 import pandas as pd
-import datetime
+import time 
+import xlsxwriter
 
 ports = serial.tools.list_ports.comports() # Reads ports in computer
 serialInst = serial.Serial() # Arduino
@@ -39,10 +40,10 @@ layout = [
     [sg.Text(portListStrings, key='-PORTLIST-')],
     [sg.Text('Select Com Port Number', key='-SCPN-'), sg.Combo(portNumList, key='-COMLIST-')],  
     [sg.Button('Ok', key='-OK-', bind_return_key=True)],
-    [sg.Text("Photoresistor Value", key='-PV-', visible=False)],
-    [sg.Graph(canvas_size=(GRAPH_SIZE_X, GRAPH_SIZE_Y), graph_bottom_left=(0,0), graph_top_right=(GRAPH_SIZE_X, GRAPH_SIZE_Y), background_color='white', key='-GRAPH-')]
+    [sg.Text("Live Photoresistor Value", key='-PV-', visible=False)],
+    [sg.Graph(canvas_size=(GRAPH_SIZE_X, GRAPH_SIZE_Y), graph_bottom_left=(0,0), graph_top_right=(GRAPH_SIZE_X, GRAPH_SIZE_Y), background_color='white', key='-GRAPH-')],
+    [sg.Button('Record', key='-RECORD-', visible=False), sg.Button('Stop', key='-STOP-', visible=False, disabled=True)]
 ]
-
 
 # Create the window
 window = sg.Window("Live View Monitor", layout)
@@ -54,14 +55,34 @@ window = sg.Window("Live View Monitor", layout)
 """""""""""""""""""""""""""""""""""""""""
 EXCEL FILE FOR DATA LOG
 """""""""""""""""""""""""""""""""""""""""
-excel_file = 'datalog.xlsx'
-datalogDF = pd.read_excel(excel_file)
+# Function to create and return a new excel file everytime Record button is pressed
+def createExcelFile(filename):
+    # Create excel file
+    excel_file = xlsxwriter.Workbook(str(filename))
+    
+    # Create the worksheet
+    worksheet = excel_file.add_worksheet()
+
+    # Create the header columns
+    worksheet.write('A1', 'Time')
+    worksheet.write('B1', 'Photoresistor Value')
+
+    excel_file.close()
+    
+    # convert workbok into dataframe excel
+    xls = pd.ExcelFile(file_name)
+
+    # Returns excel file as dataframe
+    return pd.read_excel(xls)
+
+# Boolean to check Record button is pressed
+record = False
 
 """"""""""""""""""""""""""""""""""""""""""
  
 
 # Flag to check if connection with ardiuno has been made
-portCreated = False # 
+portCreated = False 
 
 while True:
     # Setup up serial connection of arduino if haven't
@@ -83,6 +104,8 @@ while True:
             window['-COMLIST-'].update(disabled=True, visible=False)
             window['-OK-'].update(disabled=True, visible=False)
             window['-PV-'].update(visible=True)
+            window['-RECORD-'].update(visible=True)
+            window['-STOP-'].update(visible=True)
 
 
     # Read serial once connection has been made
@@ -93,6 +116,29 @@ while True:
             event, values = window.read(timeout=1) # Add a time out of 1 millisecond to exit of window.read()
             if event == sg.WIN_CLOSED:
                 break
+
+            # If record button is pressed a new excel file is created
+            if event == '-RECORD-':
+                record = True
+
+                # Create dataframe to edit excel once file was created
+                file_name = time.strftime("%Y%m%d-%H%M%S") + '-LOG.xlsx'
+                datalogDF = createExcelFile(file_name)
+
+                # Update GUI buttons
+                window['-RECORD-'].update(disabled=True)
+                window['-STOP-'].update(disabled=False)
+
+            # If stop button is pressed log stops recording
+            if event == '-STOP-':
+                record = False
+
+                # Update GUI buttons
+                window['-RECORD-'].update(disabled=False)
+                window['-STOP-'].update(disabled=True)
+
+                # Writes excel sheet with new data when recording stops
+                datalogDF.to_excel(file_name, index=False)
 
             # Retrieve serial packet from arduino and update gui
             packet = serialInst.readline()
@@ -119,9 +165,10 @@ while True:
             # Draw Updated Graph
             window['-GRAPH-'].DrawLine((PREV_X_VALUE, PREV_Y_VALUE), (X_VALUE, Y_VALUE), width=1)
 
-            # Add values to excel
-            new_row = {'Time': datetime.datetime.now(), 'Photoresistor Value': Y_VALUE}
-            datalogDF.loc[len(datalogDF)] = new_row # Adds new row to excel sheet
+            # Add values to excel if record button is True
+            if record == True: 
+                new_row = {'Time': time.strftime("%Y%m%d-%H%M%S"), 'Photoresistor Value': Y_VALUE}
+                datalogDF.loc[len(datalogDF)] = new_row # Adds new row to excel sheet
             
 
             # Update PREV Value for X, and Y
@@ -130,8 +177,7 @@ while True:
             # Increment X value one pixel to the right
             X_VALUE += 1
 
-# Writes excel sheet with new data when exiting program
-datalogDF.to_excel(excel_file, index=False) 
+ 
 
 window.close() # Close window once user closes window and exit program
 
